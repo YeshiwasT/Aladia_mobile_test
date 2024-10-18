39481d0fc0641b805425411caf68256df18e1892
Yeshiwas Tikimu <yeshiwastikimu0@gmail.com>
2024-10-18 22:20:29 +0300

    widgetbook github workflow

---
+++ lib/plus_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlusIcon extends StatelessWidget {
+  const PlusIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hello"),
+    );
+  }
+}
